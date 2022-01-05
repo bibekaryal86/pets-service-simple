5fe8f0234d2791c1eac7fe31fe4b1b69ad787ca7
Bibek Aryal <bibekaryal86@gmail.com>
2022-01-05 14:37:03 -0700

    Update ServletFilter Logging

--- app/src/main/java/pets/service/app/filter/ServletFilter.java
+++ app/src/main/java/pets/service/app/filter/ServletFilter.java
@@ -8,35 +8,44 @@ import pets.service.app.util.Util;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 public class ServletFilter implements Filter {
 
+    private static final String TRACE = "TRACE";
     private static final List<String> DO_NOT_FILTER = List.of(Util.CONTEXT_PATH + "/tests/ping");
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        logRequest(httpServletRequest);
 
-        if (DO_NOT_FILTER.contains(httpServletRequest.getRequestURI()) ||
-                Util.isAuthenticatedRequest(httpServletRequest)) {
-            chain.doFilter(request, response);
-            logResponse(httpServletRequest, httpServletResponse);
+        if ("/favicon.ico".equals(httpServletRequest.getRequestURI())) {
+            httpServletResponse.setStatus(200);
         } else {
-            httpServletResponse.setCharacterEncoding("utf-8");
-            httpServletResponse.setContentType("application/json");
-            httpServletResponse.setStatus(401);
-            httpServletResponse.getWriter().print("{\"errMsg\": \"Error! Authorization Missing!! Please Try Again!!!\"}");
-            logResponse(httpServletRequest, httpServletResponse);
+            httpServletRequest.setAttribute(TRACE, ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE));
+            logRequest(httpServletRequest);
+
+            if (DO_NOT_FILTER.contains(httpServletRequest.getRequestURI()) ||
+                    Util.isAuthenticatedRequest(httpServletRequest)) {
+                chain.doFilter(request, response);
+                logResponse(httpServletRequest, httpServletResponse);
+            } else {
+                httpServletResponse.setCharacterEncoding("utf-8");
+                httpServletResponse.setContentType("application/json");
+                httpServletResponse.setStatus(401);
+                httpServletResponse.getWriter().print("{\"errMsg\": \"Error! Authorization Missing!! Please Try Again!!!\"}");
+                logResponse(httpServletRequest, httpServletResponse);
+            }
         }
     }
 
     private void logRequest(HttpServletRequest httpServletRequest) {
-        log.info("REQUEST BEGIN: [ {} ] | [ {} ]", httpServletRequest.getRequestURI(), httpServletRequest.getRemoteAddr());
+        log.info("[ {} ] | REQUEST::: Incoming: [ {} ] | Method: [ {} ]",
+                httpServletRequest.getAttribute(TRACE), httpServletRequest.getRequestURI(), httpServletRequest.getMethod());
     }
 
     private void logResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        log.info("REQUEST END: [ {} ] | [ {} ]", httpServletRequest.getRequestURI(), httpServletResponse.getStatus());
+        log.info("[ {} ] | RESPONSE::: Status [ {} ]", httpServletRequest.getAttribute(TRACE), httpServletResponse.getStatus());
     }
 }
